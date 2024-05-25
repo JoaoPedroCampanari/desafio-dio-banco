521ef22a083b3cf53c42816719a735b2cdc1f165
Joaofcampanari <joaofcampanari@gmail.com>
2024-05-25 14:52:10 -0300

    Versao stantard

---
+++ src/Conta.java
@@ -0,0 +1,41 @@
+public abstract class Conta implements RegrasConta{
+    private static int AGENCIA_PADRAO = 470970;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.saldo = 0;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+
+    }
+}
---
+++ src/RegrasConta.java
@@ -0,0 +1,8 @@
+public interface RegrasConta {
+
+      void sacar(double valor);
+
+      void depositar(double valor);
+
+      void transferir(double valor, Conta contaDestino);
+}
